d74116b0ebee325d6ade012592b75bf89f5157c2
vfcoelho <vfcoelho18.2@hotmail.com>
2025-05-16 14:21:31 -0300

    Auto commit for Studio handoff

--- models/canvas/Untitled.sql
+++ models/canvas/Untitled.sql
@@ -1,12 +1,41 @@
-WITH stg__leases AS (
-  /* Staged leases data from our property management warehouse. */
+WITH stg__properties AS (
+  /* Staged properties data from our property management warehouse. */
   SELECT
     *
-  FROM {{ ref('property_management', 'stg__leases') }}
+  FROM {{ ref('property_management', 'stg__properties') }}
+), stg__units AS (
+  /* Staged units data from our property management warehouse. */
+  SELECT
+    *
+  FROM {{ ref('property_management', 'stg__units') }}
+), join_1 AS (
+  SELECT
+    *
+  FROM stg__units
+  JOIN stg__properties
+    ON stg__units.PROPERTY_ID = stg__properties.ID
+), aggregate_1 AS (
+  SELECT
+    STATUS,
+    COUNT(STATUS) AS count_STATUS
+  FROM join_1
+  GROUP BY
+    STATUS
+), filter_1 AS (
+  SELECT
+    *
+  FROM aggregate_1
+  WHERE
+    STATUS IN ('vacant', 'rented')
+), rename_1 AS (
+  SELECT
+    *
+    RENAME (STATUS AS situation, count_STATUS AS amount)
+  FROM filter_1
 ), untitled_sql AS (
   SELECT
     *
-  FROM stg__leases
+  FROM rename_1
 )
 SELECT
   *
