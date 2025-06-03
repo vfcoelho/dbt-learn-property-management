07f9a0c5cf024f549e634bcc11105ffba6849880
vfcoelho <vfcoelho18.2@hotmail.com>
2025-06-03 18:47:06 -0300

    tests

--- models/marts/dim_properties.sql
+++ models/marts/dim_properties.sql
@@ -1,26 +1,37 @@
-with
-    properties as (select * from {{ ref("stg__properties") }}),
-    units as (select * from {{ ref("stg__units") }}),
-    final as (
-        select
-            p.id,
-            p.name,
-            p.address,
-            u.address_2,
-            p.city,
-            p.state,
-            p.zip_code,
-            p.type,
-            p.year_built,
-            u.id as unit_id,
-            u.rent,
-            u.bedrooms,
-            u.bathrooms,
-            u.square_feet,
-            u.status,
-            p.created_at,
-        from properties p
-        inner join units u on u.property_id = p.id
-    )
-select *
-from final
+WITH stg__properties AS (
+  SELECT
+    *
+  FROM {{ ref('stg__properties') }}
+), stg__units AS (
+  SELECT
+    *
+  FROM {{ ref('stg__units') }}
+), join_05c8 AS (
+  SELECT
+    stg__units.ID AS UNIT_ID,
+    stg__properties.ID,
+    stg__properties.NAME,
+    stg__properties.ADDRESS,
+    stg__units.ADDRESS_2,
+    stg__properties.CITY,
+    stg__properties.STATE,
+    stg__properties.ZIP_CODE,
+    stg__properties.TYPE,
+    stg__properties.YEAR_BUILT,
+    stg__units.RENT,
+    stg__units.BEDROOMS,
+    stg__units.BATHROOMS,
+    stg__units.SQUARE_FEET,
+    stg__units.STATUS,
+    stg__properties.CREATED_AT
+  FROM stg__properties
+  JOIN stg__units
+    ON stg__properties.ID = stg__units.PROPERTY_ID
+), dim_properties AS (
+  SELECT
+    *
+  FROM join_05c8
+)
+SELECT
+  *
+FROM dim_properties
