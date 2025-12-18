a7ed5504a2a9439aecd5e732803e012d402f8ad1
vfcoelho <vfcoelho18.2@hotmail.com>
2025-12-18 12:01:19 -0300

    test edit file path

--- models/marts/fact_leases_v2.sql
+++ models/marts/fact_leases_v2.sql
@@ -1,23 +1,99 @@
-with
-
-    leases as (select * from {{ ref("stg__leases") }}),
-    units as (select * from {{ ref("stg__units") }}),
-    rental_applications as (select * from {{ ref("stg__rental_applications") }}),
-    unit_lease_count as (
-        select u.id, count(*) as unit_leases_count
-        from leases l
-        inner join units u on l.unit_id = u.id
-        group by u.id
-    ),
-
-    final as (
-        select l.id as lease_id, u.property_id, u.id as unit_id, ra.id as rental_application_id, ulc.unit_leases_count,
-        l.start_date::timestamp as start_date, l.end_date::timestamp as end_date, l.completed as completed, u.rent as rent
-        from leases l
-        inner join units u on l.unit_id = u.id
-        inner join unit_lease_count ulc on ulc.id = u.id
-        inner join rental_applications ra on ra.id = l.rental_application_id
-    )
-
-select *
-from final
+WITH stg__leases AS (
+  SELECT
+    *
+  FROM {{ ref('stg__leases') }}
+), stg__units AS (
+  SELECT
+    *
+  FROM {{ ref('stg__units') }}
+), stg__rental_applications AS (
+  SELECT
+    *
+  FROM {{ ref('stg__rental_applications') }}
+), rename_2 AS (
+  SELECT
+    *
+    RENAME (ID AS L_ID, CREATED_AT AS L_CREATED_AT, UPLOADED_AT AS L_UPLOADED_AT)
+  FROM stg__leases
+), rename_5 AS (
+  SELECT
+    *
+    RENAME (ID AS L_ID, CREATED_AT AS L_CREATED_AT, UPLOADED_AT AS L_UPLOADED_AT)
+  FROM stg__leases
+), rename_3 AS (
+  SELECT
+    *
+    RENAME (ID AS U_ID, CREATED_AT AS U_CREATED_AT, UPLOADED_AT AS U_UPLOADED_AT)
+  FROM stg__units
+), rename_6 AS (
+  SELECT
+    *
+    RENAME (ID AS U_ID, CREATED_AT AS U_CREATED_AT, UPLOADED_AT AS U_UPLOADED_AT, STATUS AS U_STATUS)
+  FROM stg__units
+), rename_1 AS (
+  SELECT
+    *
+    RENAME (ID AS RA_ID, CREATED_AT AS RA_CREATED_AT, UPLOADED_AT AS RA_UPLOADED_AT, STATUS AS RA_STATUS)
+  FROM stg__rental_applications
+), join_1 AS (
+  SELECT
+    *
+  FROM rename_2
+  JOIN rename_3
+    ON rename_2.UNIT_ID = rename_3.U_ID
+), join_2 AS (
+  SELECT
+    *
+  FROM rename_5
+  JOIN rename_6
+    ON rename_5.UNIT_ID = rename_6.U_ID
+), aggregate_1 AS (
+  SELECT
+    U_ID,
+    COUNT(*) AS UNIT_LEASES_COUNT
+  FROM join_1
+  GROUP BY
+    U_ID
+), rename_4 AS (
+  SELECT
+    U_ID AS ULC_ID,
+    UNIT_LEASES_COUNT
+  FROM aggregate_1
+), join_3 AS (
+  SELECT
+    *
+  FROM join_2
+  JOIN rename_4
+    ON join_2.U_ID = rename_4.ULC_ID
+), join_4 AS (
+  SELECT
+    *
+  FROM join_3
+  JOIN rename_1
+    ON join_3.RENTAL_APPLICATION_ID = rename_1.RA_ID
+), formula_1 AS (
+  SELECT
+    *,
+    CAST(START_DATE AS TIMESTAMP) AS START_DATE,
+    CAST(END_DATE AS TIMESTAMP) AS END_DATE
+  FROM join_4
+), rename_7 AS (
+  SELECT
+    L_ID AS LEASE_ID,
+    PROPERTY_ID,
+    U_ID AS UNIT_ID,
+    RA_ID AS RENTAL_APPLICATION_ID,
+    UNIT_LEASES_COUNT,
+    START_DATE AS BEGIN_DATE,
+    END_DATE,
+    COMPLETED,
+    RENT
+  FROM formula_1
+), fact_leases_v2 AS (
+  SELECT
+    *
+  FROM rename_7
+)
+SELECT
+  *
+FROM fact_leases_v2
